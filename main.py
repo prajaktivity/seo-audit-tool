ff48128887e33c73da5298ca210685814583e76d
Prajakta Wankhede <prajakta12wankhede@gmail.com>
2026-04-17 23:35:18 +0530

    cors added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes import seo, keyword
 from routes import recommendations
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (for development)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(seo.router)
 app.include_router(keyword.router)
